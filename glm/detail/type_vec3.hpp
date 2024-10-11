99dda73139e15eda4d20079d2f8dfd93cd1ea5ec
Zuzu-Typ <Zuzu.Typ@gmail.com>
2024-10-11 11:57:17 +0200

    Revert "Simd improvement"
    This reverts commit 803eddb7bb31501ba77ba41f83f4f36ea6f8274f.

--- glm/detail/type_vec3.hpp
+++ glm/detail/type_vec3.hpp
@@ -22,11 +22,6 @@ namespace glm
 		typedef vec<3, T, Q> type;
 		typedef vec<3, bool, Q> bool_type;
 
-		enum is_aligned
-		{
-			value = detail::is_aligned<Q>::value
-		};
-
 		// -- Data --
 
 #		if GLM_SILENT_WARNINGS == GLM_ENABLE
@@ -257,8 +252,6 @@ namespace glm
 		GLM_FUNC_DECL GLM_CONSTEXPR vec<3, T, Q> & operator>>=(vec<3, U, Q> const& v);
 	};
 
-
-
 	// -- Unary operators --
 
 	template<typename T, qualifier Q>
@@ -435,10 +428,6 @@ namespace glm
 
 	template<qualifier Q>
 	GLM_FUNC_DECL GLM_CONSTEXPR vec<3, bool, Q> operator||(vec<3, bool, Q> const& v1, vec<3, bool, Q> const& v2);
-
-
-
-
 }//namespace glm
 
 #ifndef GLM_EXTERNAL_TEMPLATE
